2a99b84140d43e6b8adcf346123529285dc954a5
aris <v.b.konidaris@gmail.com>
2019-10-01 18:10:04 +0300

    Interactive OML branch

--- src/main/scala/INFORE/protocol/safeAsynchronousProto.scala
+++ src/main/scala/INFORE/protocol/safeAsynchronousProto.scala
@@ -1,14 +1,13 @@
 package INFORE.protocol
 
 import INFORE.learners.Learner
-import INFORE.logic.{psAsyncLogic, safeWorkerAsyncLogic, workerAsyncLogic}
+import INFORE.logic.{psAsyncLogic, safeWorkerAsyncLogic}
 import INFORE.message.LearningMessage
 import INFORE.parameters.LearningParameters
 
 class safeAsynchronousProto[L <: Learner]()
   extends safeLearningProto[LearningMessage, (Int, Int, LearningParameters), LearningMessage, L] {
   override def workerLogic: safeWorkerAsyncLogic[L] = new safeWorkerAsyncLogic[L]
-
   override def psLogic: psAsyncLogic = new psAsyncLogic
 }
 
